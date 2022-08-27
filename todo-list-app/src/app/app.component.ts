f9a8683d56cc3ae49893b2a2e98e36ca09ed772f
soabel <alfredo.benaute@gmail.com>
2022-08-26 20:25:10 -0500

    fix: add todo features

--- todo-list-app/src/app/app.component.ts
+++ todo-list-app/src/app/app.component.ts
@@ -8,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'todo-list-app';
 
+  list: any[] = [];
+  newTask: any = {};
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+    this.list.push({ id: 1, name: 'Limpiar habitación' });
+    this.list.push({ id: 2, name: 'Lectura diaria' });
+
+  }
+
+  add(): void {
+    if (!this.newTask.name)
+      return;
+    this.list.push(this.newTask);
+    this.newTask = {};
+  }
+  delete(item: any): void {
+    let index = this.list.findIndex(x => x.name == item.name);
+
+    console.log('index', index);
+
+    // delete this.list[index];
+    this.list.splice(index,1);
+
   }
 
 }
